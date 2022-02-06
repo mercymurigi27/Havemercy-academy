2c4945307af632ca1c65ddd18277e1d2abeda044
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-02-06 17:36:37 +0300

    add background align items

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const myQuestions = [
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+            a: "Node.js",
+            b: "TypeScript",
+            c: "npm"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            a: "Angular",
+            b: "jQuery",
+            c: "RequireJS",
+            d: "ESLint"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            a: "ordered list",
+            b: "menu list",
+            c: "foodlist",
+            d: "directory list"
+        },
+        correctAnswer: "c"
+    ]
