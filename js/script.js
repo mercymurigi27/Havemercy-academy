58f0064161543d0ae6d2cd7e7544f0746ea004df
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-02-08 14:15:40 +0300

    defined variable on js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-var correctAns = {
-    HTML: "D",
-    jvas: "D",
-    CODE:"A",
-    inbuilt:"D",
-    strin:"A",
-    funi:"c",
-    obj:"c",
-    bol:"D",
-    arr:"A",
-    shift:"B"
+function check(){
+    var c = 0;
+    var q1 = document.quiz.question1.value;
+    var q2 = document.quiz.question2.value;
+    var q3 = document.quiz.question3.value;
+    var q4 = document.quiz.question4.value;
+    var q5 = document.quiz.question5.value;
+    var q6 = document.quiz.question6.value;
+    var q7 = document.quiz.question7.value;
+    var q8 = document.quiz.question8.value;
+    var q9 = document.quiz.question9.value;
+    var q10 = document.quiz.question10.value;
 
 }
 
-let fomu = document.getElementById("fomu");
-let score = 0;
-fomu.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var userData = new FormData(fomu);
-    compareData(userData);
-});
